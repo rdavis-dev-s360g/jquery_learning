5348bfb0f71c29f5091fa6cdd0a5d88650861756
thedukeofny666 <eatthis1>
2016-02-03 10:47:37 -0600

    JWT example

---
+++ jwt/app.js
@@ -0,0 +1,33 @@
+
+//https://github.com/kjur/jsrsasign/wiki#programming-tutorial
+
+// load up the app
+$(function() {
+    $("#encode").click(function() {
+        createJWT();
+    });
+});
+
+function createJWT() {
+    // Header
+    var oHeader = {alg: 'HS256', typ: 'JWT'};
+
+    // Payload
+    var oPayload = {};
+    var tNow = KJUR.jws.IntDate.get('now');
+    var tEnd = KJUR.jws.IntDate.get('now + 1day');
+    oPayload.iss = "http://www.source360group.com";
+    oPayload.sub = "mailto:rdavis@source360group.com";
+    oPayload.nbf = tNow;
+    oPayload.iat = tNow;
+    oPayload.exp = tEnd;
+    oPayload.jti = "id123456";
+    oPayload.aud = "http://foo.com/employee";
+
+    // Sign JWT, password=616161
+    var sHeader = JSON.stringify(oHeader);
+    var sPayload = JSON.stringify(oPayload);
+    var sJWT = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, "secret2");
+    $("#encodedValue").text(sJWT);
+}
+
